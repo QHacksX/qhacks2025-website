03ed9c6639b4b30bbf1817c4527bd7601d81c13e
KosiSochi Amobi-Oleka <kosi.amobioleka@gmail.com>
2024-06-03 21:59:08 -0300

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-
+import Donut from "@/components/shared/3d-models/donut";
 
 export default function Home() {
   return (
-    <main></main>
+    <main>
+      <Donut />
+    </main>
   );
 }
--- components/shared/3d-models/donut.tsx
+++ components/shared/3d-models/donut.tsx
@@ -1,1 +1,37 @@
-// TODO
+"use client";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import * as THREE from 'three';
+
+
+function RotatingTorus() {
+  const torusRef = useRef<THREE.Mesh>(null!);
+
+  useFrame(() => {
+    if (torusRef.current) {
+      torusRef.current.rotation.x += 0.01;
+      torusRef.current.rotation.y += 0.01;
+    }
+  });
+
+  return (
+    <mesh ref={torusRef}>
+      <torusGeometry args={[2, 1, 30, 100]} />
+      <meshBasicMaterial color="black" transparent={true} opacity={0.5} />
+    </mesh>
+  );
+}
+
+export default function Donut() {
+  return (
+    <div style={{ height: "100vh" }}>
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <RotatingTorus />
+        <OrbitControls />
+      </Canvas>
+    </div>
+  );
+}
