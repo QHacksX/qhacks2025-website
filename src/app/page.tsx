b6812fcb4c001f37488850c21cc4abf429a294e4
Daniel Wagner <58407467+Wags15@users.noreply.github.com>
2024-06-09 13:03:29 -0400

    feat: scroll lock
    TODO: behaviour is not amazing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,42 @@
+"use client";
+import { useEffect, useState } from "react";
+import { BsChevronDoubleDown, BsChevronDoubleUp } from "react-icons/bs";
 
+import useDetectScroll, {
+  Axis,
+  Direction,
+} from "@smakss/react-scroll-direction";
 
 export default function Home() {
+  const { scrollDir } = useDetectScroll();
+
+  useEffect(() => {
+    if (scrollDir === Direction.Down)
+      window.scrollTo({ top: 1000, behavior: "smooth" });
+    if (scrollDir === Direction.Up) {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  }, [scrollDir]);
   return (
-    <main>
+    <main className='overflow-hidden'>
+      <div className='h-screen flex justify-center items-end pb-10'>
+        <a
+          onClick={() => {
+            window.scrollTo({ top: 1000, behavior: "smooth" });
+          }}
+        >
+          <BsChevronDoubleDown size={100} />
+        </a>
+      </div>
+      <div className='h-screen flex justify-center items-start pt-10'>
+        <a
+          onClick={() => {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+          }}
+        >
+          <BsChevronDoubleUp size={100} />
+        </a>
+      </div>
     </main>
-  ); 
+  );
 }
