9e0bc2af53ad34162019c1b0710f2b91cb61a876
Daniel Wagner <58407467+Wags15@users.noreply.github.com>
2024-06-12 13:53:46 -0400

    fix: style on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,17 @@ import { BsChevronDoubleDown, BsChevronDoubleUp } from "react-icons/bs";
 import Footer from "../components/shared/footer";
 import InvertedWaveBackground from "../components/shared/header";
 
-import Styles from "@/src/css/style.module.css";
-
 import useDetectScroll, { Direction } from "@smakss/react-scroll-direction";
 import React from "react";
 
 import { useRouter, useSearchParams } from "next/navigation";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase/config";
+import Link from "next/link";
 
 export default function Home() {
-  const searchParams = useSearchParams();
-
   const router = useRouter();
+
   const [screenWidth, setScreenWidth] = useState(0);
 
   const [isSignedIn, setIsSignedIn] = useState(auth.currentUser !== null);
@@ -48,10 +46,6 @@ export default function Home() {
 
   const { scrollDir } = useDetectScroll();
 
-  const handleOnClick = () => {
-    router.push("/interest-form");
-  };
-
   useEffect(() => {
     if (scrollDir === Direction.Down)
       window.scrollTo({ top: 1000, behavior: "smooth" });
@@ -68,9 +62,7 @@ export default function Home() {
         screenWidth={screenWidth}
       />
       <div className='h-screen flex justify-center items-center w-screen flex-col'>
-        <h1
-          className={`md:w-1/3 text-center text-6xl font-bold leading-relaxed ${Styles["text-shadow-big"]}`}
-        >
+        <h1 className='md:w-1/3 text-center text-6xl font-bold leading-relaxed text-shadow-big'>
           Get Ready <p className='text-3xl'>FOR</p> QHacks 2025!
         </h1>
         <a
@@ -92,21 +84,19 @@ export default function Home() {
           <BsChevronDoubleUp size={100} />
         </a>
         <div className='md:w-1/3 text-center flex justify-center flex-col px-3'>
-          <h1
-            className={`text-center text-4xl font-bold leading-relaxed ${Styles["text-shadow-big"]}`}
-          >
+          <h1 className='text-center text-4xl font-bold leading-relaxed text-shadow-big'>
             Come Join Us!
           </h1>
           <p className='text-xl py-5'>
             Interested in joining? Click the button below to pre-register!
           </p>
-          <button
-            onClick={handleOnClick}
-            className={`mt-3 shadow-[0_0_20px_1px_rgb(255,255,255)] bg-blue-900 py-3 rounded-lg border border-[#ffd24d] hover:text-[#ff4040] font-bold`}
+          <Link
+            href='/interest-form'
+            className='mt-3 shadow-[0_0_20px_1px_rgb(255,255,255)] bg-blue-900 py-3 rounded-lg border border-[#ffd24d] hover:text-[#ff4040] font-bold'
           >
             {/* eslint-disable-next-line react/no-unescaped-entities */}
             I'm Interested!
-          </button>
+          </Link>
         </div>
         <div className='justify-self-end w-full'>
           <Footer />
