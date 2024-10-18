ac8b4ca66330dee527a85f7e9e8c6c635b1bcd84
spy772 <edward.tanurkov@gmail.com>
2024-10-18 14:14:39 -0400

    feat: base for previous sponsors rotating list

--- src/components/shared/sponsors/currentSponsors.tsx
+++ src/components/shared/sponsors/currentSponsors.tsx
@@ -49,3 +49,4 @@ export default function CurrentSponsors() {
         </div>
     );
 }
+
--- src/components/shared/sponsors/pastSponsors.tsx
+++ src/components/shared/sponsors/pastSponsors.tsx
@@ -1,13 +1,36 @@
-
+import { motion } from "framer-motion";
+import SponsorBubble from "./sponsorBubble";
 
 export default function PastSponsors() {
+    let orbitDuration = 30;
+    const sponsors = [
+        "/crown2.png"
+    ]
+
     return (
-        <h1 className="align-left text-white text-3xl md:text-5xl mb-2 md:mb-0 font-bold ">
-            Join Us
-            <span className="text-transparent text-3xl md:text-5xl bg-clip-text bg-gradient-to-r from-red-500 to-orange-600">
-                {" "}
-                In Person!
-            </span>
-        </h1>
+        <div className="pt-10 justify-center items-center w-screen flex flex-col">
+            <div className="pb-10">
+                <h1 className="mb-3 text-white  mx-5 text-3xl md:text-5xl font-bold">
+                    Highlighting Our{" "}
+                    <span className="text-transparent text-3xl md:text-5xl bg-clip-text bg-gradient-to-r from-red-500 to-orange-600">
+                        Previous Sponsors
+                    </span>
+                </h1>
+            </div>
+
+            <motion.div className="absolute w-[300px] h-[300px] rounded-full"
+                style={{ border: '2px dashed rgba(255, 255, 255, 0.5)' }}
+                animate={{ rotate: 360 }}
+                transition={{
+                    repeat: Infinity,
+                    duration: orbitDuration, // Define duration for full rotation
+                    ease: 'linear',
+                }}
+            >
+                {[...sponsors].map((sponsor) => (
+                    <SponsorBubble sponsor={sponsor} />
+                ))}
+            </motion.div>
+        </div>
     )
 }
