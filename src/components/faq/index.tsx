2d6216b6da96248c1e684594278563da716a01d7
Daniel Wagner <58407467+Wags15@users.noreply.github.com>
2024-10-20 19:36:57 -0400

    feat: moving clouds

--- src/components/faq/index.tsx
+++ src/components/faq/index.tsx
@@ -2,6 +2,8 @@
 import { useWindowSize } from "@/src/hooks/useWindowSize";
 import { ContactForm } from "./contact";
 import Dropdown from "./dropdown";
+import Cloud from "./cloud";
+import "../../css/FaqStyle.css";
 
 export default function Faq() {
   const size = useWindowSize();
@@ -10,18 +12,18 @@ export default function Faq() {
     size.height == undefined ? 800 : size.height * 0.55
   );
 
-  console.log(bottomPadding.toString());
-
   return (
     <div className=' text-black w-screen flex flex-col bg-brand-light-blue'>
       <div className='pt-10 bg-gradient-to-b from-black to-brand-light-blue w-full h-64' />
-      <div className='bg-transparent absolute w-full items-center justify-center flex flex-col z-10 pt-64'>
+
+      <div className='bg-transparent absolute w-full items-center justify-center flex flex-col z-20 pt-96'>
         <h1 className='align-center  font-extrabold text-7xl py-12'>FAQ's</h1>
         <Dropdown />
         <ContactForm />
       </div>
-
+      <Cloud />
       <div style={{ height: bottomPadding ? `${bottomPadding}px` : "400px" }} />
+
       <img
         src={"/pond-footer.png"}
         alt='Pond With Ducks'
