d2a33207af0a575dc130c050ced65e46a0139858
spy772 <edward.tanurkov@gmail.com>
2024-10-20 23:30:11 -0400

    feat: sponsors things nice-looking

--- src/components/shared/sponsors/sponsorBubble.tsx
+++ src/components/shared/sponsors/sponsorBubble.tsx
@@ -6,19 +6,19 @@ export default function SponsorBubble({
     sponsor: Sponsor
 }) {
     return (
-        <div className={`sponsor-bubble bubble-${sponsor.colour}`}>
-            <div className='sponsor-bubble-img-container'>
-                <img width={112} height={112} id='sponsor_logo' src={sponsor.logo} className="rounded-full w-28 h-28 sponsor-bubble-img" />
-            </div>
+        <div className="grid justify-items-center">
+            <div className={`sponsor-bubble bubble-${sponsor.colour}`}>
+                <div className='sponsor-bubble-img-container'>
+                    <img width={112} height={112} id='sponsor_logo' src={sponsor.logo} className="rounded-full w-28 h-28 sponsor-bubble-img" />
+                </div>
 
-            <div className={`sponsor-bubble-back`}>
-                <div className='sponsor-bubble-name'>{sponsor.name}</div>
-                <div className='sponsor-bubble-button-container'>
-
-
+                <div className={`sponsor-bubble-back`}>
+                    <div className='sponsor-bubble-button-container' />
                 </div>
             </div>
+            <div className='text-center text-l font-bold'>{sponsor.name}</div>
         </div>
+
     )
 }
 
