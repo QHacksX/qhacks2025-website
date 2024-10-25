622b7edee6d2a9b83517666960c77342c83adaeb
Daniel Wagner <58407467+Wags15@users.noreply.github.com>
2024-10-25 18:02:30 -0400

    feat: add landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,92 +43,98 @@ export default function Home() {
   return (
     <main className='overflow-hidden relative'>
       <div className='radial-gradient-background'>
-        <NavBar />
+        <div className='h-screen flex flex-col justify-center items-center w-screen pt-40 '>
+          <NavBar />
 
-        <div className='absolute inset-0 -z-20'>
-          <DonutScene x={4} y={2.5} radius={0.5} color='white' />
-        </div>
-        <div className='absolute inset-0 -z-20'>
-          <DonutScene x={-4} y={2} radius={1} color='white' />
-        </div>
-        <div className='absolute inset-0 -z-20'>
-          <DonutScene x={-4} y={-1.25} radius={0.75} color='white' />
-        </div>
-        <div className='absolute inset-0 -z-20'>
-          <DonutScene x={4} y={-0.75} radius={1.15} color='white' />
-        </div>
-
-        <img
-          src={"/qhacks_crown.svg"}
-          alt='Queens Crown'
-          width={200}
-          height={100}
-          className='w-1/2 md:w-1/3 lg:w-1/4 h-auto absolute top-24 md:top-20 min-w-[150px]'
-        />
-        <h1 className='text-center text-4xl md:text-5xl lg:text-7xl font-extrabold font-montserrat leading-relaxed mt-2 md:mt-4'>
-          QHACKS
-        </h1>
-        <p className='md:w-3/5 lg:w-1/3 text-center text-xl mt-4'>
-          Get ready to innovate and make a difference! Join QHacks, the ultimate
-          hackathon experience.
-        </p>
-        <p className='font-bold text-[#E2A022] pt-5'>In-person! [add dates]</p>
-
-        <motion.button
-          className='w-3/5 md:w-1/3 lg:w-1/5 p-3 mt-4 font-bold text-xl text-white bg-red-500 rounded-full'
-          whileHover={{ scale: 1.1 }}
-          transition={{ type: "spring", stiffness: 300 }}
-        >
-          Register Here
-        </motion.button>
-
-        <p className='md:w-1/2 lg:w-1/4 text-center text-lg mt-2'>
-          Already have an account?{" "}
-          <a href='/signin' className='text-blue-500 hover:text-blue-700'>
-            Log in
-          </a>
-        </p>
-
-        <div className='flex items-center justify-center space-x-6 mt-4'>
-          <Link href=''>
-            <img
-              src={"/tiktok_logo.svg"}
-              alt='Tiktok'
-              width={40}
-              height={40}
-              className='w-10 h-10'
-            />
-          </Link>
-          <Link href='https://www.instagram.com/qhacksx/'>
-            <img
-              src={"/ig_logo.svg"}
-              alt='Instagram'
-              width={40}
-              height={40}
-              className='w-10 h-10'
-            />
-          </Link>
-          <Link href='https://www.facebook.com/QHacks/'>
-            <img
-              src={"/facebook_logo.svg"}
-              alt='Facebook'
-              width={40}
-              height={40}
-              className='w-10 h-10'
-            />
-          </Link>
-        </div>
-      </div>
-      <div className='flex justify-between items-center w-screen flex-col select-none'>
-        <div className='w-[90%] md:w-[80%] lg:w-[70%]'>
-          <TriColor />
-          <div className='pt-20'>
-            <GrowYourNetwork />
+          <div className='z-0'>
+            <div className='absolute inset-0'>
+              <DonutScene x={4} y={2.5} radius={0.5} color='white' />
+            </div>
+            <div className='absolute inset-0'>
+              <DonutScene x={-4} y={2} radius={1} color='white' />
+            </div>
+            <div className='absolute inset-0'>
+              <DonutScene x={-4} y={-1.25} radius={0.75} color='white' />
+            </div>
+            <div className='absolute inset-0'>
+              <DonutScene x={4} y={-0.75} radius={1.15} color='white' />
+            </div>
           </div>
-          <PastSpeakers />
+
+          <img
+            src={"/qhacks_crown.svg"}
+            alt='Queens Crown'
+            // width={200}
+            // height={100}
+            className='w-1/2 md:w-1/3 lg:w-1/4 h-auto min-w-[150px]'
+          />
+          <h1 className='text-center text-4xl md:text-5xl lg:text-7xl font-extrabold font-montserrat leading-relaxed mt-2 md:mt-4'>
+            QHACKS
+          </h1>
+          <p className='md:w-3/5 lg:w-1/3 text-center text-xl mt-4'>
+            Get ready to innovate and make a difference! Join QHacks, the
+            ultimate hackathon experience.
+          </p>
+          <p className='font-bold text-[#E2A022] pt-5'>
+            In-person! [add dates]
+          </p>
+
+          <motion.button
+            className='w-3/5 md:w-1/3 lg:w-1/5 p-3 mt-4 font-bold text-xl text-white bg-red-500 rounded-full z-20'
+            whileHover={{ scale: 1.1 }}
+            transition={{ type: "spring", stiffness: 300 }}
+          >
+            Register Here
+          </motion.button>
+
+          <p className='md:w-1/2 lg:w-1/4 text-center text-lg mt-2 z-20'>
+            Already have an account?{" "}
+            <a
+              href='/signin'
+              className='text-blue-500 hover:text-blue-700 z-20'
+            >
+              Log in
+            </a>
+          </p>
+
+          <div className='flex items-center justify-center space-x-6 mt-4 z-20'>
+            <Link href=''>
+              <img
+                src={"/tiktok_logo.svg"}
+                alt='Tiktok'
+                width={40}
+                height={40}
+                className='w-10 h-10'
+              />
+            </Link>
+            <Link href='https://www.instagram.com/qhacksx/'>
+              <img
+                src={"/ig_logo.svg"}
+                alt='Instagram'
+                width={40}
+                height={40}
+                className='w-10 h-10'
+              />
+            </Link>
+            <Link href='https://www.facebook.com/QHacks/'>
+              <img
+                src={"/facebook_logo.svg"}
+                alt='Facebook'
+                width={40}
+                height={40}
+                className='w-10 h-10'
+              />
+            </Link>
+          </div>
         </div>
-        <div>
-          <Faq />
+        <div className='flex justify-between items-center w-screen flex-col select-none'>
+          <div className='w-[90%] md:w-[80%] lg:w-[70%]'>
+            <TriColor />
+            <div className='pt-20'>
+              <GrowYourNetwork />
+            </div>
+            <PastSpeakers />
+          </div>
         </div>
       </div>
 
