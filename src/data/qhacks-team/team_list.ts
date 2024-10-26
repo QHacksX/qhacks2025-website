c292ad403b4afe3d48a09346dcaa2510a15547c3
spy772 <edward.tanurkov@gmail.com>
2024-10-25 20:54:32 -0400

    feat: team member section
    - No images, css is being funny

---
+++ src/data/qhacks-team/team_list.ts
@@ -0,0 +1,245 @@
+export const qhacksTeam = [
+    {
+        "name": "Ilyas Erdogan",
+        "role": "Co-Chair",
+        "instagram": "https://www.instagram.com/ilyas_egn/",
+        "image": "",
+        "color": "bg-red-500",
+        "linkedin": "https://www.linkedin.com/in/ilyas-erdogan/",
+        "github": "https://github.com/Ilyas-Erdogan"
+    },
+    {
+        "name": "Joash Mathew",
+        "role": "Co-Chair",
+        "instagram": "https://www.instagram.com/j0ashm/",
+        "image": "",
+        "color": "bg-yellow-300",
+        "linkedin": "https://www.linkedin.com/in/j0ashm/",
+        "github": "https://github.com/j0ashm"
+    },
+    {
+        "name": "Steven Zhang",
+        "role": "Co-Chair",
+        "instagram": "https://www.instagram.com/steven._.z/",
+        "image": "",
+        "color": "bg-blue-500",
+        "linkedin": "https://www.linkedin.com/in/steventzhang/",
+        "github": "https://github.com/StevenO7"
+    },
+    {
+        "name": "Jonathan Wilson",
+        "role": "Finance Director",
+        "instagram": "https://www.instagram.com/jonathan_wilson14/",
+        "image": "",
+        "color": "bg-red-500",
+        "linkedin": "https://www.linkedin.com/in/jonathan-wilson-70487621b/",
+        "github": null
+    },
+    {
+        "name": "Adam Raco",
+        "role": "Partnerships Director",
+        "instagram": "https://www.instagram.com/adam.raco/",
+        "image": "",
+        "color": "bg-yellow-300",
+        "linkedin": "https://www.linkedin.com/in/adam-raco/",
+        "github": "https://github.com/TheBugAssassin"
+    },
+    {
+        "name": "Amanda Cao",
+        "role": "Partnerships Officer",
+        "instagram": "https://www.instagram.com/amandasmyz/",
+        "image": "",
+        "color": "bg-blue-500",
+        "linkedin": "https://www.linkedin.com/in/amanda-cao-b214a2290/",
+        "github": "https://github.com/amandacao2"
+    },
+    {
+        "name": "Meagan Liu",
+        "role": "Partnerships Officer",
+        "instagram": "https://www.instagram.com/meaganliu_/",
+        "image": "",
+        "color": "bg-red-500",
+        "linkedin": "https://www.linkedin.com/in/meagan-liu",
+        "github": "https://github.com/meaganliu"
+    },
+    {
+        "name": "Edward Tanurkov",
+        "role": "Technology Director",
+        "instagram": "https://www.instagram.com/edster28/",
+        "image": "",
+        "color": "bg-yellow-300",
+        "linkedin": "https://www.linkedin.com/in/edward-tanurkov",
+        "github": "https://github.com/spy772"
+    },
+    {
+        "name": "Daniel Wagner",
+        "role": "Technology Director",
+        "instagram": "https://www.instagram.com/daniel_wagner15/",
+        "image": "",
+        "color": "bg-blue-500",
+        "linkedin": "https://www.linkedin.com/in/daniel-wagner15/",
+        "github": "https://github.com/Wags15"
+    },
+    {
+        "name": "Robbie Huang",
+        "role": "Technology Officer",
+        "instagram": "https://www.instagram.com/robbie_h2/",
+        "image": "",
+        "color": "bg-red-500",
+        "linkedin": "https://www.linkedin.com/in/robbie-huang-/",
+        "github": null
+    },
+    {
+        "name": "Adwait Srivastava",
+        "role": "Technology Officer",
+        "instagram": "https://www.instagram.com/sri_adwait/",
+        "image": "",
+        "color": "bg-yellow-300",
+        "linkedin": "https://www.linkedin.com/in/adwait-srivastava",
+        "github": null
+    },
+    {
+        "name": "Kosi Amobi-Oleka",
+        "role": "Technology Officer",
+        "instagram": "https://www.instagram.com/kosi359/",
+        "image": "",
+        "color": "bg-blue-500",
+        "linkedin": "https://www.linkedin.com/in/kosi-amobi-oleka-51557225a/",
+        "github": "https://github.com/WebOps2"
+    },
+    {
+        "name": "Nicholas Lykopoulos",
+        "role": "Operations Director",
+        "instagram": "https://www.instagram.com/nicholaslyko/",
+        "image": "",
+        "color": "bg-red-500",
+        "linkedin": "http://linkedin.com/in/nicholas-lykopoulos-13565423a",
+        "github": null
+    },
+    {
+        "name": "Suhaib Khan",
+        "role": "Operations Officer",
+        "instagram": "https://www.instagram.com/_suhaibskhan/",
+        "image": "",
+        "color": "bg-yellow-300",
+        "linkedin": "https://www.linkedin.com/in/suhaibsadequekhan/",
+        "github": null
+    },
+    {
+        "name": "Navya Bajwa",
+        "role": "Operations Officer",
+        "instagram": "https://www.instagram.com/navyabajwa/",
+        "image": "",
+        "color": "bg-blue-500",
+        "linkedin": "https://www.linkedin.com/in/navyabajwa",
+        "github": null
+    },
+    {
+        "name": "Dajung Yoon",
+        "role": "Logistics Director",
+        "instagram": "https://www.instagram.com/dajungyoon_24/",
+        "image": "",
+        "color": "bg-red-500",
+        "linkedin": null,
+        "github": "https://github.com/DajungYoon"
+    },
+    {
+        "name": "Andrew Terry",
+        "role": "Logistics Director",
+        "instagram": "https://www.instagram.com/mangothebirb/",
+        "image": "",
+        "color": "bg-yellow-300",
+        "linkedin": "https://www.linkedin.com/in/andrew-terry-364ba2206/",
+        "github": "https://github.com/MangoTheBirb"
+    },
+    {
+        "name": "Kevin Chen",
+        "role": "Logistics Officer",
+        "instagram": "https://www.instagram.com/chenzkc1024/",
+        "image": "",
+        "color": "bg-blue-500",
+        "linkedin": "https://www.linkedin.com/in/chenzkc/ ",
+        "github": null
+    },
+    {
+        "name": "Shyan Vilva",
+        "role": "Logistics Officer",
+        "instagram": "shyan_vilva",
+        "image": "",
+        "color": "bg-red-500",
+        "linkedin": "https://www.linkedin.com/in/shyanvilva/",
+        "github": null
+    },
+    {
+        "name": "Ryan Tobia",
+        "role": "Logistics Officer",
+        "instagram": "https://www.instagram.com/ryantobia/",
+        "image": "",
+        "color": "bg-yellow-300",
+        "linkedin": null,
+        "github": "https://github.com/RyanTobia"
+    },
+    {
+        "name": "Ana Coelho",
+        "role": "Marketing Director",
+        "instagram": "https://www.instagram.com/anarcxo_/",
+        "image": "",
+        "color": "bg-blue-500",
+        "linkedin": null,
+        "github": "https://github.com/aninha0836"
+    },
+    {
+        "name": "Grace Anyu Sun",
+        "role": "Marketing Officer",
+        "instagram": "https://www.instagram.com/sun._.grace/",
+        "image": "",
+        "color": "bg-red-500",
+        "linkedin": "https://www.linkedin.com/in/isaacfungg/",
+        "github": null
+    },
+    {
+        "name": "Sue Oliveros Chahin",
+        "role": "Marketing Officer",
+        "instagram": "https://www.instagram.com/Sueidaoliveross/",
+        "image": "",
+        "color": "bg-yellow-300",
+        "linkedin": null,
+        "github": null
+    },
+    {
+        "name": "Isaac Fung",
+        "role": "First-Year Rep.",
+        "instagram": "https://www.instagram.com/isaacfungg_/",
+        "image": "",
+        "color": "bg-blue-500",
+        "linkedin": null,
+        "github": null
+    },
+    {
+        "name": "Elizabeth Bighiu",
+        "role": "First-Year Rep.",
+        "instagram": "https://www.instagram.com/elizabeth.bighiu/",
+        "image": "",
+        "color": "bg-red-500",
+        "linkedin": "www.linkedin.com/in/elizabethbighiu",
+        "github": null
+    },
+    {
+        "name": "Jasmine Qin",
+        "role": "First-Year Rep.",
+        "instagram": "https://www.instagram.com/jasm_qin/",
+        "image": "",
+        "color": "bg-yellow-300",
+        "linkedin": "https://www.linkedin.com/in/jasmine-qin-5a241820a/",
+        "github": null
+    },
+    {
+        "name": "Ela Aydiner",
+        "role": "First-Year Rep.",
+        "instagram": "https://www.instagram.com/ela_292/",
+        "image": "",
+        "color": "bg-blue-500",
+        "linkedin": "www.linkedin.com/in/ela-aydiner-31247422b ",
+        "github": "https://github.com/ela202"
+    }
+]
