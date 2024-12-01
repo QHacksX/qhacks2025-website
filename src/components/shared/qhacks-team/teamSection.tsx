54a0a29e8b21a0e11288ac0ad4950e306b4582c5
spy772 <edward.tanurkov@gmail.com>
2024-11-30 20:28:39 -0500

    feat: team section

--- src/components/shared/qhacks-team/teamSection.tsx
+++ src/components/shared/qhacks-team/teamSection.tsx
@@ -3,7 +3,7 @@ import { qhacksTeam } from "@/src/data/qhacks-team/team_list";
 
 const TeamSection = () => {
     return (
-        <div className="pb-100">
+        <div className="pb-100 mb-100 pt-20">
             <div className="pb-10">
                 <h1 className="align-center text-white text-3xl md:text-5xl mb-2 md:mb-0 font-bold text-center">
                     <span className="text-transparent text-3xl md:text-5xl bg-clip-text bg-gradient-to-r from-red-500 to-orange-600">
