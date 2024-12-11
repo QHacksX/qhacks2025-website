b907bd9899ab880dfe08078799342f9020d96151
Daniel Wagner <58407467+Wags15@users.noreply.github.com>
2024-12-11 17:20:19 -0500

    feat: update logos

--- src/data/sponsors/sponsors_lists.ts
+++ src/data/sponsors/sponsors_lists.ts
@@ -16,7 +16,7 @@ export type sponsorBlurb = {
 export const sponsorBlurbs: sponsorBlurb[] = [
   {
     "name": "Manulife Financial",
-    "logo": "/sponsors/blurbs/manulife.png",
+    "logo": "/sponsors/manulife.png",
     "link": "https://www.manulife.ca/careers.html",
     "blurb":
       "Manulife is a leading global financial services institution dedicated to helping individuals and businesses achieve their financial goals through a variety of services, including insurance, wealth, and asset management. With a presence in numerous markets worldwide, we are committed to nurturing the next generation of talent. We offer co-op, internship, and new graduate programs for students across diverse fields such as actuarial science, technology, digital marketing, investment, Global Wealth and Asset Management (GWAM), accounting and finance, and more. Join us to gain valuable experience, develop your skills, and launch your career with a company that values innovation, growth, and excellence.",
@@ -40,9 +40,9 @@ export const currentSponsors: sponsorType[] = [
   },
   {
     "name": "Manulife Financial",
-    "logo": "/sponsors/Manulife-WhiteBG.png",
+    "logo": "/sponsors/manulife.png",
     "link": "https://www.manulife.ca/careers.html",
-    "scale": 1.75,
+    "scale": 1,
     "tier": "giga",
   },
   // Doesn't seem like we have them this year
@@ -82,11 +82,25 @@ export const currentSponsors: sponsorType[] = [
     "tier": "mega",
   },
   {
+    "name": "GitHub",
+    "logo": "/sponsors/github.png",
+    "link": "https://github.com/about/careers",
+    "scale": 1.6,
+    "tier": "kilo",
+  },
+  {
     "name": "CPP",
     "logo": "/sponsors/CPP.svg",
     "link":
       "https://www.canada.ca/en/services/benefits/publicpensions/cpp.html",
     "scale": 1.25,
+    "tier": "kilo",
+  },
+  {
+    "name": "Accenture",
+    "logo": "/sponsors/accenture.png",
+    "link": "https://www.accenture.com/ca-en",
+    "scale": 1.2,
     "tier": "kilo",
   },
   {
@@ -136,20 +150,6 @@ export const currentSponsors: sponsorType[] = [
     "logo": "/sponsors/Wolfram.png",
     "link": "https://www.wolfram.com/index.en.html",
     "scale": 0.9,
-    "tier": "kilo",
-  },
-  {
-    "name": "Accenture",
-    "logo": "/sponsors/accenture.png",
-    "link": "https://www.accenture.com/ca-en",
-    "scale": 1.2,
-    "tier": "kilo",
-  },
-  {
-    "name": "GitHub",
-    "logo": "/sponsors/github.png",
-    "link": "https://github.com/about/careers",
-    "scale": 1.6,
     "tier": "kilo",
   },
 ];
@@ -533,7 +533,7 @@ export const fullPrevSponsors = [
   },
   {
     "name": "Manulife Financial",
-    "logo": "/sponsors/ManuLife.svg",
+    "logo": "/sponsors/manulife.png",
     "link": "https://www.manulife.ca/careers.html",
     "colour": "bg-yellow-500",
     "tier": "mega",
