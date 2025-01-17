c6df6424f8772268a5ef8709ded740018a127410
Daniel Wagner <58407467+Wags15@users.noreply.github.com>
2025-01-17 12:01:18 -0500

    feat: qt blurb

--- src/data/sponsors/sponsors_lists.ts
+++ src/data/sponsors/sponsors_lists.ts
@@ -18,7 +18,15 @@ export const sponsorBlurbs: sponsorBlurb[] = [
     "name": "National Bank of Canada",
     "logo": "/sponsors/NationalBankLogoNew.png",
     "link": "https://emplois.bnc.ca/fr_CA/careers",
-    "blurb": "With $462 billion in assets as at October 31, 2024, National Bank of Canada is one of Canada's six systemically important banks. The Bank has approximately 30,000 employees in knowledge-intensive positions and operates through three business segments in Canada: Personal and Commercial Banking, Wealth Management and Financial Markets. A fourth segment, U.S. Specialty Finance and International, complements the growth of its domestic operations. Its securities are listed on the Toronto Stock Exchange (TSX: NA). Follow the Bank's activities at nbc.ca or via social media.\n\nNational Bank Financial Markets offers a complete suite of products and services to corporations, institutional clients, and public-sector entities. The segment focuses on providing comprehensive advisory services and research or capital markets products and services to support clients and their growth. National Bank Financial Markets’ structure fosters agility and collaboration while encouraging creativity.",
+    "blurb":
+      "With $462 billion in assets as at October 31, 2024, National Bank of Canada is one of Canada's six systemically important banks. The Bank has approximately 30,000 employees in knowledge-intensive positions and operates through three business segments in Canada: Personal and Commercial Banking, Wealth Management and Financial Markets. A fourth segment, U.S. Specialty Finance and International, complements the growth of its domestic operations. Its securities are listed on the Toronto Stock Exchange (TSX: NA). Follow the Bank's activities at nbc.ca or via social media.\n\nNational Bank Financial Markets offers a complete suite of products and services to corporations, institutional clients, and public-sector entities. The segment focuses on providing comprehensive advisory services and research or capital markets products and services to support clients and their growth. National Bank Financial Markets’ structure fosters agility and collaboration while encouraging creativity.",
+  },
+  {
+    "name": "Qt Group",
+    "logo": "/sponsors/QtGroup.png",
+    "link": "https://www.qt.io/group",
+    "blurb":
+      "We are a global software powerhouse, trusted by industry leaders and over 1.5 million developers worldwide. Our solutions drive productivity across the entire product development journey—from intuitive UI design and agile development to seamless quality management and deployment. Qt is where software excellence begins.",
   },
   {
     "name": "Manulife Financial",
@@ -26,7 +34,7 @@ export const sponsorBlurbs: sponsorBlurb[] = [
     "link": "https://www.manulife.ca/careers.html",
     "blurb":
       "Manulife is a leading global financial services institution dedicated to helping individuals and businesses achieve their financial goals through a variety of services, including insurance, wealth, and asset management. With a presence in numerous markets worldwide, we are committed to nurturing the next generation of talent. We offer co-op, internship, and new graduate programs for students across diverse fields such as actuarial science, technology, digital marketing, investment, Global Wealth and Asset Management (GWAM), accounting and finance, and more. Join us to gain valuable experience, develop your skills, and launch your career with a company that values innovation, growth, and excellence.",
-  },
+  }
 ];
 
 export const currentSponsors: sponsorType[] = [
