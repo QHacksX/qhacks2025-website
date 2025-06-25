89229a41aeff5d07eb175bc42073f83965626359
kwon-michael <90662213+kwon-michael@users.noreply.github.com>
2025-06-25 12:57:53 -0400

    commented application form

--- src/components/application-form/dropdownInput.tsx
+++ src/components/application-form/dropdownInput.tsx
@@ -1,61 +1,61 @@
-import { Dropdown } from "primereact/dropdown";
-import { Dispatch, SetStateAction } from "react";
-import {
-  DropdownTypes,
-  dropdownOptions,
-} from "@/src/data/dropdown-options/options";
-import React from "react";
+// import { Dropdown } from "primereact/dropdown";
+// import { Dispatch, SetStateAction } from "react";
+// import {
+//   DropdownTypes,
+//   dropdownOptions,
+// } from "@/src/data/dropdown-options/options";
+// import React from "react";
 
-/**
- * Dropdown component for a given DropdownType.
- * @param type - the type of dropdown
- * @param value - the value to display
- * @setValue - setter for the value
- * @returns
- */
-export default function DropdownInput({
-  title,
-  type,
-  value,
-  setValue,
-}: {
-  title: string
-  type: DropdownTypes;
-  value: any;
-  setValue: Dispatch<SetStateAction<any>>;
-}) {
-  // We get the config for the dropdown by the type
-  const dropdownConfig = dropdownOptions.get(type);
+// /**
+//  * Dropdown component for a given DropdownType.
+//  * @param type - the type of dropdown
+//  * @param value - the value to display
+//  * @setValue - setter for the value
+//  * @returns
+//  */
+// export default function DropdownInput({
+//   title,
+//   type,
+//   value,
+//   setValue,
+// }: {
+//   title: string
+//   type: DropdownTypes;
+//   value: any;
+//   setValue: Dispatch<SetStateAction<any>>;
+// }) {
+//   // We get the config for the dropdown by the type
+//   const dropdownConfig = dropdownOptions.get(type);
 
-  const itemTemplate = (item: any) => {
-    return (
-      <div className='bg-white p-4 text-black hover:bg-gray-300'>
-        {item.value}
-      </div>
-    );
-  };
+//   const itemTemplate = (item: any) => {
+//     return (
+//       <div className='bg-white p-4 text-black hover:bg-gray-300'>
+//         {item.value}
+//       </div>
+//     );
+//   };
 
-  return (
-    <div>
-      <label
-        htmlFor='phone'
-        className='block mb-2 text-xl font-medium text-gray-300 '
-      >
-        {title}
-      </label>
-      <Dropdown
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        options={dropdownConfig?.options}
-        optionLabel={dropdownConfig?.label}
-        placeholder={dropdownConfig?.placeholder}
-        className='w-min md:w-14rem text-lg font-medium text-gray-300 ml-5'
-        filter={type === DropdownTypes.age ? false : true}
-        itemTemplate={itemTemplate}
-        filterPlaceholder='Search'
-        emptyFilterMessage={()=><></>}
-        resetFilterOnHide={true}
-      />
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <label
+//         htmlFor='phone'
+//         className='block mb-2 text-xl font-medium text-gray-300 '
+//       >
+//         {title}
+//       </label>
+//       <Dropdown
+//         value={value}
+//         onChange={(e) => setValue(e.target.value)}
+//         options={dropdownConfig?.options}
+//         optionLabel={dropdownConfig?.label}
+//         placeholder={dropdownConfig?.placeholder}
+//         className='w-min md:w-14rem text-lg font-medium text-gray-300 ml-5'
+//         filter={type === DropdownTypes.age ? false : true}
+//         itemTemplate={itemTemplate}
+//         filterPlaceholder='Search'
+//         emptyFilterMessage={()=><></>}
+//         resetFilterOnHide={true}
+//       />
+//     </div>
+//   );
+// }
--- src/components/application-form/wordInput.tsx
+++ src/components/application-form/wordInput.tsx
@@ -1,34 +1,34 @@
-import React from "react";
-import { Dispatch, SetStateAction } from "react";
+// import React from "react";
+// import { Dispatch, SetStateAction } from "react";
 
-export default function WordInput({
-  title,
-  input,
-  setInput,
-  placeholder,
-}: {
-  title: string;
-  input: string;
-  setInput: Dispatch<SetStateAction<string>>;
-  placeholder: string;
-}) {
-  return (
-    <div>
-      <label
-        htmlFor={input}
-        className='block mb-2 text-xl font-medium text-gray-300 '
-      >
-        {title}
-      </label>
-      <input
-        type='text'
-        id='wordInput'
-        className='bg-gray-50 border-b border-gray-300 text-gray-300 text-base font-medium  block w-full p-2.5 bg-transparent focus:ring-0 focus:outline-none'
-        onChange={(e) => setInput(e.target.value)}
-        value={input}
-        autoComplete='off'
-        // placeholder={placeholder}
-      />
-    </div>
-  );
-}
+// export default function WordInput({
+//   title,
+//   input,
+//   setInput,
+//   placeholder,
+// }: {
+//   title: string;
+//   input: string;
+//   setInput: Dispatch<SetStateAction<string>>;
+//   placeholder: string;
+// }) {
+//   return (
+//     <div>
+//       <label
+//         htmlFor={input}
+//         className='block mb-2 text-xl font-medium text-gray-300 '
+//       >
+//         {title}
+//       </label>
+//       <input
+//         type='text'
+//         id='wordInput'
+//         className='bg-gray-50 border-b border-gray-300 text-gray-300 text-base font-medium  block w-full p-2.5 bg-transparent focus:ring-0 focus:outline-none'
+//         onChange={(e) => setInput(e.target.value)}
+//         value={input}
+//         autoComplete='off'
+//         // placeholder={placeholder}
+//       />
+//     </div>
+//   );
+// }
